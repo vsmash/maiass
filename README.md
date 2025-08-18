e75b94d587e3a760b143c9dc925579cc0693e177
vsmsh <vsmash@users.noreply.github.com>
2025-08-18 13:04:01 +1000

    Release v5.8.18 distribution files

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ![MAIASS Banner](https://raw.githubusercontent.com/vsmash/maiass/main/assets/maiassbanner2.png)
 
 <div align="center">
-# MAIASS v5.8.16
+# MAIASS v5.8.18
 Modular AI-Augmented Semantic Scribe
 
 ---
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# MAIASS Installation Script v5.8.16
+# MAIASS Installation Script v5.8.18
 # Installs maiass.sh to a system location and creates a symlink in PATH
 # use brew if osx or this for linux
 set -e
